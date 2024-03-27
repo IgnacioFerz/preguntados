12b8a03a3716a34403935d6c4bd2701251080592
IgnacioFerz <ignacio.fernandez.fernandez@iesjulianmarias.es>
2024-03-27 13:29:43 +0100

    pr

--- migrations/Version20240327120931.php
+++ migrations/Version20240327120931.php
@@ -14,7 +14,7 @@ final class Version20240327120931 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+         return '';
     }
 
     public function up(Schema $schema): void
