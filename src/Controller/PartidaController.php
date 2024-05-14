5eb36bb1146baea62660730d740ad731ec618c8d
Ignacio <nfernandez@quadram.mobi>
2024-05-14 19:14:01 +0200

    Protect routes

--- src/Controller/PartidaController.php
+++ src/Controller/PartidaController.php
@@ -12,7 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class PartidaController extends AbstractController
 {
     private PartidaRepository $partidaRepository;
